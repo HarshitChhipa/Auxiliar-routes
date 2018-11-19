9ef98520c351fd8728d4d90ccb5ce6dc2cd3f556
HarshitChhipa <hchippa@expedien.in>
2018-11-19 10:38:36 +0530

    first commmit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {OutletComponentComponent} from './outlet-component/outlet-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: OutletComponentComponent,
+    outlet: 'outlibeti'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OutletComponentComponent } from './outlet-component/outlet-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OutletComponentComponent
   ],
   imports: [
     BrowserModule,
